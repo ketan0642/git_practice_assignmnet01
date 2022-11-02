cdfeaa53afe915bebae931ff1632adefa95ad450
ketan0642 <tawharesonu0@gmail.com>
2022-11-02 12:27:34 +0530

    Prime Problem

---
+++ prime.js
@@ -0,0 +1,20 @@
+function checkPrime(number){ 
+  let factors=0;
+  for(let i=1; i<=num; i++){
+     if(number % i ==0 ){
+         factors++ ; 
+     }
+  }
+   if(factors == 2 ){
+        return true; 
+   }
+    return false;
+}
+
+let answer = checkPrime(13); 
+
+if(answer == true){
+    console.log("Prime Number");
+} else {
+    console.log("Not a Prime Number");
+
